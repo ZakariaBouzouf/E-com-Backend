dcd1cdf0657335d1631c0316d42b3a26483aacfc
Zakaria Bouzouf <zakaria.bouzouf@gmail.com>
2024-09-25 12:30:05 +0200

    Adding the support of accepting request from the client.

--- src/main/java/com/dezo/e_com/EComApplication.java
+++ src/main/java/com/dezo/e_com/EComApplication.java
@@ -2,12 +2,27 @@ package com.dezo.e_com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EComApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EComApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EComApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("http://localhost:3000");
+      }
+    };
+  }
 
 }
